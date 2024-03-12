d39f8f0027549ff21c26c997010e8f34791b649a
Robert Kalmar <rfrostkalmar@gmail.com>
2024-03-12 09:51:09 +0100

    Reorder cmp and delete unused nvim config dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -351,10 +351,10 @@ cmp.setup({
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		-- { name = 'vsnip' }, -- For vsnip users.
-		{ name = 'luasnip' }, -- For luasnip users.
 		{ name = 'nvim_lsp' },
 		{ name = 'nvim_lsp_signature_help' },
+		-- { name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
 		-- { name = 'snippy' }, -- For snippy users.
 	}, {
