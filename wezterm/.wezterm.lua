f50f7cdec01742f21fc2350745a537568b65cb58
Robert Kalmar <rfrostkalmar@gmail.com>
2024-03-26 20:16:43 +0100

    Helix changes, added wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font "FiraCode Nerd Font Mono"
+config.color_scheme = "Catppuccin Mocha"
+return config
+
